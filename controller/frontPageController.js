c3a18021e67ee3a4ef8b38fb952fce2ab05790f7
Tina Wendpaap <tw222eu@student.lnu.se>
2021-03-23 18:03:35 +0100

    getting groups from gitlab, adding some style

--- controller/frontPageController.js
+++ controller/frontPageController.js
@@ -1,20 +1,13 @@
 const frontPageController = {}
-const fetch = require('node-fetch')
+// const fetch = require('node-fetch')
 
 frontPageController.index = async (req, res) => {
-  const token = req.session.token
-  console.log(token)
-  const fetchGroups = await fetch('https://gitlab.lnu.se/api/v4/groups?min_access_level=30', {
-    headers: { access_token: token }
-  })
-  const result = await fetchGroups.json()
-  const groups = await result.map(group => ({
-    name: group.full_name
-  }))
+  // console.log(token)
+
   // const projects = await result.map(group => ({
   //   name: group.name_with_namespace
   // }))
-  console.log(groups)
-  res.render('home', { token })
+
+  res.render('home')
 }
 module.exports = frontPageController
--- controller/gitlabController.js
+++ controller/gitlabController.js
@@ -1,28 +1,53 @@
 const axios = require('axios')
 const gitlabController = {}
+const fetch = require('node-fetch')
 
-gitlabController.index = async (req, res) => {
-  if (req.headers['x-gitlab-token'] === 'ajhhdii8726jdjdkdjs') {
-    console.log(req.body.object_attributes)
-    res.status(200)
-  }
-}
+// gitlabController.index = async (req, res) => {
+//   if (req.headers['x-gitlab-token'] === 'ajhhdii8726jdjdkdjs') {
+//     console.log(req.body.object_attributes)
+//     res.status(200)
+//   }
+// }
 
 gitlabController.login = async (req, res) => {
   res.redirect(`https://gitlab.lnu.se/oauth/authorize?client_id=${process.env.APPLICATION_ID}&redirect_uri=http://013f49ab95b3.ngrok.io/auth&response_type=code&state=STATE&scope=api`)
 }
 
 gitlabController.auth = async (req, res) => {
-  const parameters = `client_id=${process.env.APPLICATION_ID}&client_secret=${process.env.SECRET}&code=${req.query.code}&grant_type=authorization_code&redirect_uri=${process.env.LINK}`
+  const parameters = `client_id=${process.env.APPLICATION_ID}&client_secret=${process.env.SECRET}&code=${req.query.code}&grant_type=authorization_code&redirect_uri=http://013f49ab95b3.ngrok.io/auth`
   axios.post('https://gitlab.lnu.se/oauth/token', parameters)
     .then((_res) => _res.data.access_token)
     .then((token) => {
-      // console.log('My token:', token)
-      // localStorage.setItem('token', token)
+      console.log('My token:', token)
       req.session.token = token
-      res.redirect('/')
+      res.redirect('/selectGroup')
     })
-  // res.redirect('/auth')
-  // console.log(req.session.token)
+}
+
+gitlabController.selectGroup = async (req, res) => {
+  const token = req.session.token
+  const fetchGroups = await fetch('https://gitlab.lnu.se/api/v4/groups?min_access_level=30', {
+    headers: { Authorization: `Bearer ${token}` }
+  })
+  // console.log(token)
+  const result = await fetchGroups.json()
+  const groups = await result.map(group => ({
+    name: group.full_name,
+    id: group.id
+  }))
+  // req.session.ids = { id: groups.map(element => element.id) }
+  // console.log(groups)
+  res.render('gitLab', { token, groups })
+}
+
+gitlabController.getSpecificGroup = async (req, res) => {
+  // console.log(req.params.id)
+  const token = req.session.token
+  const fetchSpec = await fetch(`https://gitlab.lnu.se/api/v4/groups/${req.params.id}/subgroups`, {
+    headers: { Authorization: `Bearer ${token}` }
+  })
+  const result = await fetchSpec.json()
+  console.log(result)
+  res.render('spec')
 }
 module.exports = gitlabController
