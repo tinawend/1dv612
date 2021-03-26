ce1cb85131c1563af13401215c542d4700c6597f
Tina Wendpaap <tw222eu@student.lnu.se>
2021-03-26 11:34:07 +0100

    save notices

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect(`${process.env.DB_LINK}`,
+mongoose.connect(`${process.env.MONGODB_URI}`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
