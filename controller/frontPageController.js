f0d536d2a2232d8c2a450cd626964155e3359ad7
Tina Wendpaap <tw222eu@student.lnu.se>
2021-03-26 12:37:13 +0100

    hbs

--- controller/frontPageController.js
+++ controller/frontPageController.js
@@ -2,12 +2,13 @@ const frontPageController = {}
 
 const Notification = require('../model/notice')
 frontPageController.index = async (req, res) => {
+  const token = req.session.token
   const notifictions = await Notification.find({})
   const todaysDate = new Date().toISOString().slice(0, 10)
   const issues = await notifictions.filter(issue => issue.updated === todaysDate).map(issue => ({
     title: issue.title,
     description: issue.description
   }))
-  res.render('home', { issues })
+  res.render('home', { issues, token })
 }
 module.exports = frontPageController
